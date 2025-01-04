8e1ed1fbf9328e97e38aa8aec0b283e36f07f9a6
Mahesh-xr <mahesh.572xr@gmail.com>
2025-01-04 23:27:16 +0530

    added roll button

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var leftRandomNumber = Math.ceil(Math.random()*6);
- var rightRandomNumber = Math.ceil(Math.random()*6);
- document.querySelector('.img1').setAttribute('src','./images/'+'dice'+leftRandomNumber+'.png');
- document.querySelector('.img2').setAttribute('src','./images/'+'dice'+rightRandomNumber+'.png');
- if(leftRandomNumber>rightRandomNumber){
-    document.querySelector('h1').innerText = "🚩Player 1 Won!";
- }
- else if(leftRandomNumber<rightRandomNumber){
-    document.querySelector('h1').innerText = "Player 2 Won!🚩";
- }
- else{
-    document.querySelector('h1').innerText = "it's Draw." 
- }
+ document.querySelector("button").addEventListener("click", function(){
+      var leftRandomNumber = Math.ceil(Math.random()*6);
+      var rightRandomNumber = Math.ceil(Math.random()*6);
+      document.querySelector('.img1').setAttribute('src','./images/'+'dice'+leftRandomNumber+'.png');
+      document.querySelector('.img2').setAttribute('src','./images/'+'dice'+rightRandomNumber+'.png');
+      if(leftRandomNumber>rightRandomNumber){
+         document.querySelector('h1').innerText = "🚩Player 1 Won!";
+      }
+      else if(leftRandomNumber<rightRandomNumber){
+         document.querySelector('h1').innerText = "Player 2 Won!🚩";
+      }
+      else{
+         document.querySelector('h1').innerText = "it's Draw." 
+      }
+ })
+ 
+ 
+ 
