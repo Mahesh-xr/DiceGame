ad71207421dfc157f35dda1da22c7b5dcfd1edf5
Mahesh-xr <mahesh.572xr@gmail.com>
2025-01-04 20:04:16 +0530

    just added few logics in js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var leftRandomNumber = Math.ceil(Math.random()*6);
  document.querySelector('.img1').setAttribute('src','./images/'+'dice'+leftRandomNumber+'.png');
  document.querySelector('.img2').setAttribute('src','./images/'+'dice'+rightRandomNumber+'.png');
  if(leftRandomNumber>rightRandomNumber){
-    document.querySelector('h1').innerText = "🚩Player 1 Won!"
+    document.querySelector('h1').innerText = "🚩Player 1 Won!";
  }
- else if(leftRandomNumber===rightRandomNumber){
-    document.querySelector('h1').innerText = "its Draw!"
+ else if(leftRandomNumber<rightRandomNumber){
+    document.querySelector('h1').innerText = "Player 2 Won!🚩";
  }
  else{
-    document.querySelector('h1').innerText = "Player 2 Won!🚩"
-
+    document.querySelector('h1').innerText = "it's Draw." 
  }
