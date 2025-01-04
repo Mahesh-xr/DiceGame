733e0d29df293eb5aaf5d85b1706b4d8707ea1ed
Mahesh-xr <mahesh.572xr@gmail.com>
2025-01-04 19:46:33 +0530

    Initial commit for DiceGame

---
+++ index.js
@@ -0,0 +1,14 @@
+var leftRandomNumber = Math.ceil(Math.random()*6);
+ var rightRandomNumber = Math.ceil(Math.random()*6);
+ document.querySelector('.img1').setAttribute('src','./images/'+'dice'+leftRandomNumber+'.png');
+ document.querySelector('.img2').setAttribute('src','./images/'+'dice'+rightRandomNumber+'.png');
+ if(leftRandomNumber>rightRandomNumber){
+    document.querySelector('h1').innerText = "🚩Player 1 Won!"
+ }
+ else if(leftRandomNumber===rightRandomNumber){
+    document.querySelector('h1').innerText = "its Draw!"
+ }
+ else{
+    document.querySelector('h1').innerText = "Player 2 Won!🚩"
+
+ }
